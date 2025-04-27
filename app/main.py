cdd25bedf60a8bd9daf94feab6a604d67293c8fb
jimmy89Li <liviu.c.simion@gmail.com>
2025-04-27 12:43:47 +0200

    Refactor main.py to integrate database session management and model usage from the database module

--- app/main.py
+++ app/main.py
@@ -1,35 +1,9 @@
 from fastapi import FastAPI, HTTPException
-from contextlib import asynccontextmanager
-from sqlmodel import Field, SQLModel, create_engine, Session, select
+from sqlmodel import Session, select
+from . import database, models
 
 
-# Database engine
-sqlite_file_name = "address_book.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-engine = create_engine(sqlite_url, echo=True)
-
-
-# Create database tables
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
-
-# @app.on_event("startup")
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    create_db_and_tables()
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
-
-
-# Data model.
-class Contact(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str
-    phone: str
-    email: str
+app = FastAPI(lifespan=database.lifespan)
 
 
 # Base route.
@@ -41,16 +15,16 @@ def index():
 # Route to get all contacts.
 @app.get("/contacts")
 def get_contacts():
-    with Session(engine) as session:
-        contacts = session.exec(select(Contact)).all()
+    with Session(database.engine) as session:
+        contacts = session.exec(select(models.Contact)).all()
         return contacts
 
 
 # Route to get one contact, by ID.
 @app.get("/contacts/{id}")
 def get_contact(id: int):
-    with Session(engine) as session:
-        contact = session.get(Contact, id)
+    with Session(database.engine) as session:
+        contact = session.get(models.Contact, id)
         if not contact:
             raise HTTPException(status_code=404, detail="Contact not found")
         return contact
@@ -58,9 +32,9 @@ def get_contact(id: int):
 
 # Route to create a new contact.
 @app.post("/contacts")
-def create_contact(new_contact: Contact):
-    with Session(engine) as session:
-        existing_contact = session.get(Contact, new_contact.email)
+def create_contact(new_contact: models.Contact):
+    with Session(database.engine) as session:
+        existing_contact = session.get(models.Contact, new_contact.email)
         if existing_contact:
             raise HTTPException(status_code=400, detail="Contact already exists.")
         session.add(new_contact)
@@ -71,9 +45,9 @@ def create_contact(new_contact: Contact):
 
 # Route to update an existing contact.
 @app.put("/contacts/{id}")
-def update_contact(id: int, updated_contact: Contact):
-    with Session(engine) as session:
-        existing_contact = session.get(Contact, id)
+def update_contact(id: int, updated_contact: models.Contact):
+    with Session(database.engine) as session:
+        existing_contact = session.get(models.Contact, id)
         if not existing_contact:
             raise HTTPException(status_code=404, detail="Contact not found")
         existing_contact.name = updated_contact.name
@@ -88,8 +62,8 @@ def update_contact(id: int, updated_contact: Contact):
 # Route to delete an existing contact.
 @app.delete("/contacts/{id}")
 def delete_contact(id: int):
-    with Session(engine) as session:
-        contact = session.get(Contact, id)
+    with Session(database.engine) as session:
+        contact = session.get(models.Contact, id)
         if not contact:
             raise HTTPException(status_code=404, detail="Contact not found")
         session.delete(contact)
