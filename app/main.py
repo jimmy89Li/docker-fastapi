e2e76d2ba653548e948d2a02dbd6d4d058a75dcb
jimmy89Li <liviu.c.simion@gmail.com>
2025-04-27 13:21:13 +0200

    Refactor contact routes in main.py to utilize CRUD functions for improved code organization and maintainability

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
-from sqlmodel import Session, select
-from . import database, models
+from sqlmodel import Session
+from . import database, models, crud
 
 
 app = FastAPI(lifespan=database.lifespan)
@@ -15,57 +15,35 @@ def index():
 # Route to get all contacts.
 @app.get("/contacts")
 def get_contacts():
-    with Session(database.engine) as session:
-        contacts = session.exec(select(models.Contact)).all()
-        return contacts
+    contacts = crud.get_contacts()
+    return contacts
 
 
 # Route to get one contact, by ID.
 @app.get("/contacts/{id}")
 def get_contact(id: int):
-    with Session(database.engine) as session:
-        contact = session.get(models.Contact, id)
-        if not contact:
-            raise HTTPException(status_code=404, detail="Contact not found")
-        return contact
+    contact = crud.get_contact(id)
+    return contact
 
 
 # Route to create a new contact.
 @app.post("/contacts")
 def create_contact(new_contact: models.Contact):
-    with Session(database.engine) as session:
-        existing_contact = session.get(models.Contact, new_contact.email)
-        if existing_contact:
-            raise HTTPException(status_code=400, detail="Contact already exists.")
-        session.add(new_contact)
-        session.commit()
-        session.refresh(new_contact)
-        return new_contact
+    contact = crud.create_contact(new_contact)
+    return contact
 
 
 # Route to update an existing contact.
 @app.put("/contacts/{id}")
 def update_contact(id: int, updated_contact: models.Contact):
-    with Session(database.engine) as session:
-        existing_contact = session.get(models.Contact, id)
-        if not existing_contact:
-            raise HTTPException(status_code=404, detail="Contact not found")
-        existing_contact.name = updated_contact.name
-        existing_contact.phone = updated_contact.phone
-        existing_contact.email = updated_contact.email
-        session.add(existing_contact)
-        session.commit()
-        session.refresh(existing_contact)
-        return existing_contact
+    existing_contact = crud.update_contact(id, updated_contact)
+    return existing_contact
 
 
 # Route to delete an existing contact.
 @app.delete("/contacts/{id}")
 def delete_contact(id: int):
-    with Session(database.engine) as session:
-        contact = session.get(models.Contact, id)
-        if not contact:
-            raise HTTPException(status_code=404, detail="Contact not found")
-        session.delete(contact)
-        session.commit()
-        return {"message": f"Contact ID: {id} deleted."}
+    delete_contact = crud.delete_contact(id)
+    if delete_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found.")
+    return delete_contact
